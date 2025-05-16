cbd0639522377875a1a3d87d292c63872cf97a19
felipehsss <felipehsacchi@gmail.com>
2025-05-16 14:34:41 -0300

    Cards adicinados

--- client/medgo/src/app/page.jsx
+++ client/medgo/src/app/page.jsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+
+  const cardInfos = [{ id: "1", titulo: "lorem, ipsum", descricao: "lorem,ipsum", path: "/home/card-img/equipe-de-jovens-medicos-especialistas-em-pe-no-corredor-hospital.jpg" }, { id: "2", titulo: "lorem, ipsum", descricao: "lorem,ipsum", path: "/home/card-img/sala-de-hospital.png" }, { id: "3", titulo: "lorem, ipsum", descricao: "lorem,ipsum", path: "/home/card-img/enfermeira-medindo-paciente-pressao-sangue.jpg" }]
   return (
     <>
       {/* Home main, titulo principal,subtitulo e imagem  */}
-      <main className="w-full justify-center gap-2 grid mt-10">
+      <main className="w-full justify-center gap-2 grid mt-32">
         <div className="  mb-4 grid gap-4  ">
           <h1 className="text-center xl:text-left md:text-center sm:text-center text-5xl titulo-cor-padrao-medgo font-semibold font-mono  "> Lorem ipsum </h1>
           <p className="text-black text-center text-sm lg:text-left   sm:w-5xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi et voluptates aspernatur dolores libero amet non reprehenderit, soluta voluptas rem, in quas eum ipsam? Quam consequatur natus ut vero quis.</p>
@@ -24,27 +26,27 @@ export default function Home() {
           <p className="text-black text-center text-sm lg:text-left   sm:w-5xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi et voluptates aspernatur dolores libero amet non reprehenderit, soluta voluptas rem, in quas eum ipsam? Quam consequatur natus ut vero quis.</p>
         </div>
 
-        <div className=" flex justify-left  w-full">
+        {/* Card */}
+        {/* card1 */}
+        <div className="grid justify-center w-full xl:flex xl:gap-3 ">
+          {cardInfos.slice(0, 3).map((cardInfo) => (
+            <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96" key={cardInfo.id}>
+              <div className="p-4">
+
+                <h6 className="mb-2 text-slate-800 text-xl font-semibold">
+                  {cardInfo.titulo}
+                </h6>
 
 
-          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <img className="rounded-t-lg" src="/home/MEDGO_logo.png" alt="" />
-            </a>
-            <div className="p-5">
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-              <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-                <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                </svg>
-              </a>
+                <p className="text-slate-600 leading-normal font-light">
+                 {cardInfo.descricao}
+                </p>
+              </div>
+              <div className="relative h-56 m-2.5 overflow-hidden text-white rounded-md">
+                <img src={cardInfo.path} alt="card-image" />
+              </div>
             </div>
-          </div>
-
+          ))}
         </div>
 
       </section>
